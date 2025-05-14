bb10fa6f8ad91bb5bd51385753c6be04bf4819ca
Hiroto1610-Y <hiroto.yaguchi0803@gmail.com>
2025-05-14 17:51:48 +0900

    import org.springframework.lang.NonNull;

--- src/main/java/com/example/todoapp/filter/JwtRequestFilter.java
+++ src/main/java/com/example/todoapp/filter/JwtRequestFilter.java
@@ -9,11 +9,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.lang.NonNull;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
@@ -24,8 +26,12 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-            throws ServletException, IOException {
+    protected void doFilterInternal(
+        @NonNull HttpServletRequest request, 
+        @NonNull HttpServletResponse response, 
+        @NonNull FilterChain chain)
+
+        throws ServletException, IOException {
 
         final String authorizationHeader = request.getHeader("Authorization");
 
