ab3a9d2473214d28bd292d060efd4e33808227bb
noritsune <bill.gatess4037@gmail.com>
2025-05-20 17:04:08 +0900

    目標一覧表画面の動作確認の為に一時的に認証不要で見られるようにした

--- src/main/java/com/example/todoapp/config/SecurityConfig.java
+++ src/main/java/com/example/todoapp/config/SecurityConfig.java
@@ -34,11 +34,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
-                .anyRequest().authenticated()
-            )
-            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .authorizeHttpRequests(auth -> auth
+                        // ユーザー認証周りの機能が未実装だが目標一覧表画面の動作を確認したいため、/tasksを入れた
+                        // TODO: ログインまでできるようになったら/tasksはここから消す
+                        .requestMatchers("/api/auth/register", "/api/auth/login", "/tasks").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
