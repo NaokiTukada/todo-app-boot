73aa2a0032862422040c7feb4ce19caa068e2a9e
NaokuTukada <nao3house7@gmail.com>
2025-05-20 18:51:27 +0900

    バックエンド側のTaskの機能をやり直しました。

--- src/main/java/com/example/todoapp/domain/Task.java
+++ src/main/java/com/example/todoapp/domain/Task.java
@@ -1,46 +1,47 @@
 package com.example.todoapp.domain;
 
-import jakarta.persistence.*;
-import lombok.Data;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
 @Entity
-@Table(name = "Task")
+@Table(name = "tasks")
 @Data
 
 public class Task {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long taskId;
 
     @ManyToOne
-    @JoinColumn(name = "user_id",   nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
-
+    
     @Column(nullable = false)
     private String title;
 
-    @Column(name = "due_date")
-    private LocalDateTime dueDate;
+    @Column(name = "schaduled_time")
+    private LocalDateTime schaduledTime;
 
-    @Column(name = "created_at", nullable = false, updatable = false)
+    @Column(name = "createdAt", nullable = false)
     private LocalDateTime createdAt;
 
-    @Column(name = "updated_at", nullable = false)
-    private LocalDateTime updatedAt;   
+    @Column(name = "completed_at")
+    private LocalDateTime completedAt;
+
+    @Column(name = "is_completed")
+    private boolean completed; 
+
+    @Column(name = "current_streak", nullable = false, columnDefinition = "INT DEFAULT 0")
+    private int currentStreak;
+
+    @Column(name = "last_streak_updated")
+    private LocalDate lastStreakUpdated;
 
     @PrePersist
-    protected void onCreate() {
-        createdAt = LocalDateTime.now();
-        updatedAt = LocalDateTime.now();
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
     }
-
-
-
-    @PreUpdate
-    protected void onUpdate() {
-        updatedAt = LocalDateTime.now();
-    }
-
 }
