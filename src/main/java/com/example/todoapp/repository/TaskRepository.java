ce8531c57a43afb64e1fbdafc9ffe8ea799800e9
NaokiTukada <nao3house7@gmail.com>
2025-05-21 13:48:55 +0900

    実行時エラーによるTaskRepository.javaの微修正

--- src/main/java/com/example/todoapp/repository/TaskRepository.java
+++ src/main/java/com/example/todoapp/repository/TaskRepository.java
@@ -9,6 +9,5 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long>{
-    List<Task> findByUserEntity(User user);
-    List<Task> findAllByUser(User user);
+    List<Task> findByUser(User user);
 }
