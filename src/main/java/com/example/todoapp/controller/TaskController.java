d211460e7e75af8c9d98b3ca0c71cbbd66dadcba
NaokiTukada <nao3house7@gmail.com>
2025-05-22 14:48:31 +0900

    Controllerもコンフリクト解消時に何かミスをしてしまったので全修正してPRを出します。

--- src/main/java/com/example/todoapp/controller/TaskController.java
+++ src/main/java/com/example/todoapp/controller/TaskController.java
@@ -1,19 +1,26 @@
 package com.example.todoapp.controller;
+
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import com.example.todoapp.domain.Task;
 import com.example.todoapp.domain.User;
 import com.example.todoapp.repository.UserRepository;
 import com.example.todoapp.service.TaskService;
+
 import lombok.RequiredArgsConstructor;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
+
     private final TaskService taskService;
     private final UserRepository userRepository;
+
     @PostMapping
     public ResponseEntity<Task> createTask(@RequestBody Task task) {
         Optional<User> userOpt = userRepository.findById(task.getUser().getUserId());
@@ -22,32 +29,38 @@ public class TaskController {
         Task savedTask = taskService.createTask(task);
         return ResponseEntity.ok(savedTask);
     }
-    @GetMapping("/{id}")
-    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
-        Optional<Task> taskOpt = taskService.getTaskById(id);
-        return taskOpt.map(ResponseEntity::ok)
-                      .orElseGet(() -> ResponseEntity.notFound().build());
-    }
+
     @GetMapping
     public List<Task> getAllTasks(@RequestParam Long userId) {
         Optional<User> userOpt = userRepository.findById(userId);
         userOpt.ifPresent(taskService::whenDateChange);
         return taskService.getAllTasks();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        Optional<Task> taskOpt = taskService.getTaskById(id);
+        return taskOpt.map(ResponseEntity::ok)
+                      .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
     @PutMapping("/{id}")
     public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
         return taskService.updateTask(id, updatedTask)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
         taskService.deleteTask(id);
         return ResponseEntity.noContent().build();
     }
+
     @PutMapping("/{id}/toggle")
     public ResponseEntity<Void> toggleTaskCompletion(@PathVariable Long id) {
         taskService.toggleTaskCompletion(id);
         return ResponseEntity.ok().build();
     }
+
 }
