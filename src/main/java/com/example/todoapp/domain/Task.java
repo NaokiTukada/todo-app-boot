0b4d3e1bed5117e56a0c87d58b6b2815ad40d018
NaokiTukada <nao3house7@gmail.com>
2025-05-22 11:23:19 +0900

    JWTとテスト実行のための環境設定追加

--- src/main/java/com/example/todoapp/domain/Task.java
+++ src/main/java/com/example/todoapp/domain/Task.java
@@ -1,44 +1,45 @@
 package com.example.todoapp.domain;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 import jakarta.persistence.*;
 import lombok.Data;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tasks")
 @Data
 
 public class Task {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long taskId;
 
     @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id",   nullable = false)
     private User user;
-    
+
     @Column(nullable = false)
     private String title;
 
-    @Column(name = "created_at", nullable = false)
+    private LocalDateTime dueDate;
+
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @Column(name = "completed_at")
-    private LocalDateTime completedAt;
-
-    @Column(name = "is_completed")
-    private boolean completed; 
-
-    @Column(name = "current_streak", nullable = false, columnDefinition = "INT DEFAULT 0")
-    private int currentStreak;
-
-    @Column(name = "last_streak_updated")
-    private LocalDate lastStreakUpdated;
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
 
     @PrePersist
-    public void prePersist() {
-        this.createdAt = LocalDateTime.now();
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
     }
+
+
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
 }
