f5dd3e13c3376422391a610df3582279c7e4356d
NaokiTukada <nao3house7@gmail.com>
2025-05-22 10:25:33 +0900

    Serviceクラスの編集、削除部分のメソッド作成

--- src/main/java/com/example/todoapp/service/TaskService.java
+++ src/main/java/com/example/todoapp/service/TaskService.java
@@ -1,12 +1,12 @@
 package com.example.todoapp.service;
 
-import com.example.todoapp.domain.Task;
-import com.example.todoapp.repository.TaskRepository;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import com.example.todoapp.domain.Task;
+import com.example.todoapp.repository.TaskRepository;
 
 @Service
 public class TaskService {
@@ -14,29 +14,36 @@ public class TaskService {
     @Autowired
     private TaskRepository taskRepository;
 
-    public Task creatTask(Task task){
-        return taskRepository.save(task);
-    }
+    //目標の追加INSERT
 
-    public  Optional<Task> getTaskById(Long id){
-        return taskRepository.findById(id);
-    }
 
-    public List<Task> getAllTasks(){
-        return taskRepository.findAll();
-    }
+    //idから目標を取得
+ 
 
-    public Task updateTask(Long id, Task updatedTask) {
-        Optional<Task> existingTask = taskRepository.findById(id);
-        if (existingTask.isPresent()) {
-            updatedTask.setTaskId(id); 
-            return taskRepository.save(updatedTask);
+    //tasksの全項目を取得する
+
+    
+    //UPDATE文(編集)
+    public Optional<Task> updateTask(Long id, Task updateTask) {
+        Optional<Task> existenceTask = taskRepository.findById(id);
+        if (existenceTask.isPresent()) {
+            updateTask.setTaskId(id);
+            return Optional.of(taskRepository.save(updateTask));
         }
-        return null; 
+        return Optional.empty();
     }
 
-    public void deleteTask(Long id) {
+    //目標の削除
+    public void deleteTask(Long id){
         taskRepository.deleteById(id);
     }
+
+    //あるユーザーが持ってるすべてのタスクを取得
+
+
+    //完了状態の時未完了状態に。未完了状態の時完了に(この時、完了時間をつける)！
+
+
+    //今日初めてログインする時に連続達成日数のカウントするメソッドと今日初めてログインするときに完了状態ならばリセットするメソッドの統合
 
 }
