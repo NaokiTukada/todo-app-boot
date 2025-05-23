fbb538e0f0612dba2313092c6d667f8065025527
Hiroto1610-Y <hiroto.yaguchi0803@gmail.com>
2025-05-23 13:20:03 +0900

    ユーザー登録ページの表示のために必要なコードを追加

--- src/main/java/com/example/todoapp/TodoappApplication.java
+++ src/main/java/com/example/todoapp/TodoappApplication.java
@@ -2,9 +2,16 @@ package com.example.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class TodoappApplication {
+	@RequestMapping("/register")
+	public String register() {
+		return "register";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoappApplication.class, args);
--- src/main/java/com/example/todoapp/config/SecurityConfig.java
+++ src/main/java/com/example/todoapp/config/SecurityConfig.java
@@ -34,10 +34,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
-                .anyRequest().authenticated()
-            )
+                .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/api/auth/register", "/api/auth/login", "/register").permitAll()
+                    .anyRequest().authenticated())
             .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
