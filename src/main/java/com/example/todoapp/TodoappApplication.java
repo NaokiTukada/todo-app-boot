62a1782c4743d5f2ec60ff44a2c4c2e89615058c
Hiroto1610-Y <hiroto.yaguchi0803@gmail.com>
2025-05-26 13:48:24 +0900

    TodoappApplication.javaに@RequestMapping("/login")を追加

--- src/main/java/com/example/todoapp/TodoappApplication.java
+++ src/main/java/com/example/todoapp/TodoappApplication.java
@@ -2,9 +2,21 @@ package com.example.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class TodoappApplication {
+	@RequestMapping("/register")
+	public String register() {
+		return "register";
+	}
+
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoappApplication.class, args);
